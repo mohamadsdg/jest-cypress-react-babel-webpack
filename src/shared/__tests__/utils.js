308e0daea9e4f7fb817aebbf3adaf0fd71802979
mohamadsdg <mohamad.r.sadeghi93@gmail.com>
2020-05-19 01:30:37 +0430

    Transpile Modules with Babel

---
+++ src/shared/__tests__/utils.js
@@ -0,0 +1,7 @@
+import {getFormattedValue} from '../utils'
+
+describe('test Utils', () => {
+  test('number format getFormattedValue', () => {
+    expect(getFormattedValue('1234.0')).toBe('1,234.0')
+  })
+})
