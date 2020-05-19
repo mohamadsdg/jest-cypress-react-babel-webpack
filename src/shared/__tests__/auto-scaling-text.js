eb71e325db58d8c768d6c4e4cea3e7608554e5c1
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-05-19 10:59:07 +0430

    Support with moduleNameMapper

---
+++ src/shared/__tests__/auto-scaling-text.js
@@ -0,0 +1,7 @@
+import {render} from '@testing-library/react'
+import React from 'react'
+import AutoScalingText from '../auto-scaling-text'
+
+test('renders', () => {
+  render(<AutoScalingText />)
+})
