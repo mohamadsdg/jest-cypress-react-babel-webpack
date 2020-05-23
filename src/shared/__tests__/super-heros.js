d8b8e23c4c221e858f2867a6bda7ed58b252471e
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-05-23 14:33:48 +0430

    Generate a Serializable Value with Jest Snapshots

--- src/shared/__tests__/super-heros.js
+++ src/shared/__tests__/super-heros.js
@@ -3,13 +3,42 @@ import {getFlyingSuperHeros} from '../../other/super-heros'
 import {} from '@testing-library/react'
 
 describe('check filter render', () => {
-  test('static', () => {
+  // test('static', () => {
+  //   const flyingHero = getFlyingSuperHeros()
+  //   // console.log(flyingHero)
+  //   expect(flyingHero).toEqual([
+  //     {name: 'Dynaguy', powers: ['disintegrationray', 'fly']},
+  //     {name: 'Apogee', powers: ['gravity control', 'fly']},
+  //     // {name: 'Jack-Jack', powers: ['shapeshifting', 'fly']},
+  //   ])
+  // })
+
+  test('should render flyingHero', () => {
     const flyingHero = getFlyingSuperHeros()
     // console.log(flyingHero)
-    expect(flyingHero).toEqual([
-      {name: 'Dynaguy', powers: ['disintegrationray', 'fly']},
-      {name: 'Apogee', powers: ['gravity control', 'fly']},
-      {name: 'Jack-Jack', powers: ['shapeshifting', 'fly']},
-    ])
+    expect(flyingHero).toMatchSnapshot()
+  })
+
+  test('should render flyingHero inline', () => {
+    const flyingHero = getFlyingSuperHeros()
+    // console.log(flyingHero)
+    expect(flyingHero).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "name": "Dynaguy",
+          "powers": Array [
+            "disintegrationray",
+            "fly",
+          ],
+        },
+        Object {
+          "name": "Apogee",
+          "powers": Array [
+            "gravity control",
+            "fly",
+          ],
+        },
+      ]
+    `)
   })
 })
