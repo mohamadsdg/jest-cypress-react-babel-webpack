f1a58ef513c5c935edcb933d7d7d3992a3484161
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-05-26 16:42:10 +0430

    config runner with linter

--- jest.config.js
+++ jest.config.js
@@ -15,5 +15,9 @@ module.exports = {
       statements: 100,
     },
   },
-  projects: ['./test/jest.client.js', './test/jest.server.js'],
+  projects: [
+    './test/jest.lint.js',
+    './test/jest.client.js',
+    './test/jest.server.js',
+  ],
 }
